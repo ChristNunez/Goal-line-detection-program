49376cc7345f1333b0e26e27aa74e2e984d75ba0
ChristNunez <145283159+ChristNunez@users.noreply.github.com>
2024-02-24 13:46:12 -0500

    Add files via upload
    Start of Python final project

---
+++ Python Final Project/Final Project.py
@@ -0,0 +1,13 @@
+# German Burset Romero, Katerine Gomez, & Christ Nunez
+# Final project / CSIT 200-01 Python Programming
+# This project is to ...
+
+
+
+def main():
+    print("Welcome to OpenCV project")
+
+
+
+if __name__ == '__main__':
+    main()
