1e502ef93155a1383f26b1d086b942cc598c7cad
kategomezb <122888992+kategomezb@users.noreply.github.com>
2024-03-08 10:31:21 -0500

    Final Project.py

--- Python Final Project/Final Project.py
+++ Python Final Project/Final Project.py
@@ -12,18 +12,17 @@ def main():
 
     vid = cv2.VideoCapture(0)
 
-    while True:
-        ret, frame = vid.read()
+    while vid.isOpened():                                   
+        ret, frame = vid.read()                         
         width = int(vid.get(3))
         height = int(vid.get(4))
 
         cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) == ord('q'):
-            break
-
-    vid.release()
-    cv2.destroyAllWindows
+            vid.release()
+            cv2.destroyAllWindows()
+            return
 
 
 if __name__ == '__main__':
