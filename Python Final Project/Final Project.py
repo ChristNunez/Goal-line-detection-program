f19315d067710c07727860b69cad04d7de89971e
ChristNunez <145283159+ChristNunez@users.noreply.github.com>
2024-03-08 02:56:25 -0500

    Update Final Project.py
    Accesses the camera and quits when pressing q

--- Python Final Project/Final Project.py
+++ Python Final Project/Final Project.py
@@ -1,12 +1,29 @@
+import cv2
+import numpy as num
+
+
 # German Burset Romero, Katerine Gomez, & Christ Nunez
 # Final project / CSIT 200-01 Python Programming
 # This project is to ...
 
-
-
 def main():
-    print("Welcome to OpenCV project")
+#    print("Welcome to OpenCV project")
+#    print(cv2.__version__)
 
+    vid = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = vid.read()
+        width = int(vid.get(3))
+        height = int(vid.get(4))
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows
 
 
 if __name__ == '__main__':
